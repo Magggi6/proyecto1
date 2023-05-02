4570432e03265377ad0a3db7dea7217f215d8a2d
margarita <margaritauwu001@gmail.com>
2023-05-02 10:44:28 -0600

    cambio de texto

--- buscar_reemplazar.py
+++ buscar_reemplazar.py
@@ -1,4 +1,4 @@
 def buscar_reemplazar(texto, buscar, reemplazar):
     return str.replace(texto, buscar, reemplazar)
 
-print(buscar_reemplazar("hola", "hola", "chao"))
+print(buscar_reemplazar("hola", "hola", "ola"))
